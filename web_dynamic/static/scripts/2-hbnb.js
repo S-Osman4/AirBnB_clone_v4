eebc3947ed82bc0d01f5ef2a731eb098bfb48898
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-29 17:46:55 +0300

    API Status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,1 +1,19 @@
+$(document).ready(function () {
+  const obj = {};
+  $('div.amenities li input').change(function () {
+    if ($(this).is(':checked')) {
+      obj[($(this).attr('data-id'))] = $(this).attr('data-name');
+    } else {
+      delete obj[($(this).attr('data-id'))];
+    }
+    $('div.amenities h4').html(Object.values(obj).join(', '));
+  });
 
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data){
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    });
+});
